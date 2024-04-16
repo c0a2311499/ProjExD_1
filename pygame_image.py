8b7c549811497a903afb0cf5fe77c003ec8e7127
Hayato.H <c0a2311499@edu.teu.ac.jp>
2024-04-16 17:48:16 +0900

    課題1

--- pygame_image.py
+++ pygame_image.py
@@ -28,8 +28,9 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1,0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1,0))
-
+            kk_rct.move_ip((+2,0))                  #課題1
+        
+        kk_rct.move_ip((-1,0))                      #課題1
         x = tmr%3200                                #練習6
         screen.blit(bg_img, [-x, 0])                #練習6
         screen.blit(bg_img2,[-x+1600,0])            #練習7-1
@@ -40,6 +41,7 @@ def main():
         pg.display.update()
         tmr += 1        
         clock.tick(200)                             #練習5
+        
 
 
 if __name__ == "__main__":
